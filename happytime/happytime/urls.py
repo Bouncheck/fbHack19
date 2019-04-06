1f5a35fbab188728ae0b37e51988dff40707b0b7
Marek Żochowski <marek@zochowski.net>
2019-04-06 15:34:27 +0200

    Add login/logout

--- happytime/happytime/urls.py
+++ happytime/happytime/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('activities/', include('activities.urls'))
+    path('activities/', include('activities.urls')),
 ]
