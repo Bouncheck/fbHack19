ec3b6b72ff2e766c11e04ea9f005dcf465edfb82
Marek Żochowski <marek@zochowski.net>
2019-04-06 16:46:49 +0200

    Add login/logout/register views
    Get rid of home app.

--- web-app/happytime/happytime/urls.py
+++ web-app/happytime/happytime/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
+from users.views import RegisterView
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
     path('activities/', include('activities.urls')),
-    path('', include('home.urls')),
+    path('', include('base.urls')),
 ]
