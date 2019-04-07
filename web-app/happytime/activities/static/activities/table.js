00075a15ad7a71f9d62d45401d72ba9f6f14be79
Bouncheck <36934780+Bouncheck@users.noreply.github.com>
2019-04-07 08:46:56 +0200

    Fix2 nieszpodziewajka.

--- web-app/happytime/activities/static/activities/table.js
+++ web-app/happytime/activities/static/activities/table.js
@@ -1,4 +1,6 @@
-$("#pop").on("click", function() {
-   $('#imagepreview').attr('src', $('#imageresource').attr('src'));
-   $('#imagemodal').modal('show'); 
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imagemodal').modal('show');   
+		});		
 });
