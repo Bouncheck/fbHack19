251da9ad1290a596cf6c3fd27dc14240c74e201a
Marek Żochowski <marek@zochowski.net>
2019-04-07 03:10:36 +0200

    Fix hosting media files

--- web-app/happytime/happytime/urls.py
+++ web-app/happytime/happytime/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from users.views import RegisterView
-
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -28,4 +29,4 @@ urlpatterns = [
     path('activities/', include('activities.urls')),
     path('', include('base.urls')),
     path('notes/',  include('notes.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web-app/happytime/notes/models.py
+++ web-app/happytime/notes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import  User
 
 
 class Note(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='screenshots/%Y/%m/%d/')
     timestamp = models.DateTimeField()
     note = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
